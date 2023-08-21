1a02a4e993ce195a22debfed3827e7afd88e2dcd
Harvey <hadycbl@gmail.com>
2023-08-21 09:18:32 -0600

    papas

--- guardar_producto.php
+++ guardar_producto.php
@@ -4,23 +4,24 @@ $user = "root";
 $pass = "";
 $db = "inventario";
 $conn = new mysqli($server, $user, $pass, $db);
-if(!$conn){
-    die("La conexion fallo: ". mysqli_connect_error());
-}else{
 
-// Obtener los datos ingresados en el formulario
-$nombre = $_POST["nombre"];
-$descripcion = $_POST["descripcion"];
-$cantidad = $_POST["cantidad"];
-$precio = $_POST["precio"];
+if (!$conn) {
+    die("La conexión falló: " . mysqli_connect_error());
+} else {
 
-// Consulta SQL para insertar los datos en la tabla "productos"
-$sql = "INSERT INTO productos (nombre, descripcion, cantidad, precio) VALUES ('$nombre', '$descripcion', $cantidad, $precio)";
+    // Obtener los datos ingresados en el formulario
+    $titulo = $_POST["titulo"];
+    $autor = $_POST["autor"];
+    $genero = $_POST["genero"];
+    $cantidad = $_POST["cantidad"];
 
-if(mysqli_query($conn, $sql)){
-    header("Location: RegistrarP.html");
-}else{
-    echo "Error: " . mysqli_error($conn);
-}
+    // Consulta SQL para insertar los datos en la tabla "libros"
+    $sql = "INSERT INTO libros (titulo, autor, genero, cantidad) VALUES ('$titulo', '$autor', $genero, $cantidad)";
+
+    if (mysqli_query($conn, $sql)) {
+        header("Location: RegistrarP.html");
+    } else {
+        echo "Error: " . mysqli_error($conn);
+    }
 }
 ?>
